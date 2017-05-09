b1c03a492c168cff062e529912201866330c0035
Antoine Musso <hashar@free.fr>
2017-05-09 12:29:16 +0200

    Remove die() from PHP entry point
    NumerAlpha.php only defines some globals, no need to die() when
    MEDIAWIKI is not defined.
    
    Change-Id: Ia54c3d7dec073624d348428c38f6b9cbe2d74f8a

--- NumerAlpha.php
+++ NumerAlpha.php
@@ -10,11 +10,6 @@
  * @license http://www.publicdomainmanifesto.org/ Public Domain
  */
 
-// Check if we are being called directly
-if ( !defined( 'MEDIAWIKI' ) ) {
-	die( 'This file is an extension to MediaWiki and thus not a valid entry point.' );
-}
-
 // Tell everybody who we are
 $GLOBALS['wgExtensionCredits']['parserhook'][] = array(
 	'path' => '__FILE__',
